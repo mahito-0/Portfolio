48bfdbb7c07a05f4b366a4d8b797d795de0548da
Syed Al Mahmud <mahmud.agni@gmail.com>
2025-09-12 21:31:11 +0600

    up

--- OTHER/msg.js
+++ OTHER/msg.js
@@ -14,30 +14,30 @@ function sendmail() {
         reply_to: email
     };
 
-  
+
     var sendButton = document.getElementById("sendLetter");
     sendButton.textContent = "Sending...";
     sendButton.disabled = true;
 
-    
+
     emailjs.send('service_zsci4of', 'template_t5wnh4c', templateParams)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
             alert("Message sent successfully!");
 
-          
+
             document.getElementById("message").value = "";
             document.getElementById("name").value = "";
             document.getElementById("email").value = "";
 
-           
+
             sendButton.textContent = "Send";
             sendButton.disabled = false;
         }, function(error) {
             console.log('FAILED...', error);
             alert("Message failed to send!");
 
-         
+
             sendButton.textContent = "Send";
             sendButton.disabled = false;
         });
